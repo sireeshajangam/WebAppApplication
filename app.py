cfe34cd4172b4625f402ee5f50b76595b205f3d2
sireeshajangam <sireesha.jangam@auropro.com>
2023-11-13 10:51:24 +0530

    committed

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 db_config = {
     'host': 'rds-db.cptyiwp2zznq.us-east-1.rds.amazonaws.com',
     'user': 'foo',
-    'password': 'f6FIVi6p<WYBs&lo2rxgjn|BdJ?c',
+    'password': 'IpA%hmRgq?M-60?u&[~KqbMq&1%y',
     'database': 'mydb',
 }
 
