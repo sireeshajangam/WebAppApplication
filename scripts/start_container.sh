df7ea0de2e9988ad11e3605a160e56266ee65d3f
sireeshajangam <sireesha.jangam@auropro.com>
2023-11-13 15:24:38 +0530

    committed

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Authenticate Docker with ECR Public in a non-interactive way
-aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/a4i5r6b0
+#aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/a4i5r6b0
 
 # Pull the Docker image from ECR Public
 docker pull public.ecr.aws/a4i5r6b0/python:latest
