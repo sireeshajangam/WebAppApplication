ab2a34a02de9aeb1814fd7bc9e3d9c40f32ef45d
sireeshajangam <sireesha.jangam@auropro.com>
2023-11-13 17:08:37 +0530

    committed

--- app.py
+++ app.py
@@ -1,63 +1,26 @@
-# File: app.py
-from flask import Flask, render_template, request, redirect, url_for
-import mysql.connector
-import os
-import sys
-print(sys.path)
-
-name = os.getenv('NAME', 'DefaultName')
-
+from flask import Flask, render_template, send_from_directory
+import json
 
 app = Flask(__name__)
 
-# Replace these values with your database connection details
-db_config = {
-    'host': 'rds-db.cptyiwp2zznq.us-east-1.rds.amazonaws.com',
-    'user': 'foo',
-    'password': 'IpA%hmRgq?M-60?u&[~KqbMq&1%y',
-    'database': 'mydb',
-}
+# Define route to serve static files (CSS, JS, etc.)
+@app.route('/static/<path:path>')
+def send_static(path):
+    return send_from_directory('static', path)
 
 # Define the route for the home page
 @app.route('/')
 def home():
-    # Connect to the database
-    conn = mysql.connector.connect(**db_config)
-    cursor = conn.cursor()
+    articles = load_articles()
+    return render_template('index.html', articles=articles)
 
-    # Retrieve and display all users
-    select_users_query = "SELECT * FROM users"
-    cursor.execute(select_users_query)
-    users = cursor.fetchall()
-
-    # Close the connection
-    cursor.close()
-    conn.close()
-
-    return render_template('index.html', users=users)
-
-# Define the route for adding a new user
-@app.route('/add_user', methods=['POST'])
-def add_user():
-    # Get user data from the form
-    username = request.form['username']
-    email = request.form['email']
-
-    # Connect to the database
-    conn = mysql.connector.connect(**db_config)
-    cursor = conn.cursor()
-
-    # Insert the new user
-    insert_user_query = "INSERT INTO users (username, email) VALUES (%s, %s)"
-    user_data = (username, email)
-    cursor.execute(insert_user_query, user_data)
-    conn.commit()
-
-    # Close the connection
-    cursor.close()
-    conn.close()
-
-    return redirect(url_for('home'))
+def load_articles():
+    try:
+        with open('articles.json', 'r') as file:
+            articles = json.load(file)
+        return articles
+    except FileNotFoundError:
+        return []
 
 if __name__ == '__main__':
-    app.run(debug=True,port=8000)
+    app.run(debug=True, port=8000)
