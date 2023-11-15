aa0f72ce4b7b10fbb1510c352b96ac8aa55e7607
sireeshajangam <sireesha.jangam@auropro.com>
2023-11-16 01:21:18 +0530

    committed

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Authenticate Docker with ECR
-/usr/local/bin/aws ecr get-login-password --region us-east-1 | /usr/bin/docker login --username AWS --password-stdin 571888835380.dkr.ecr.us-east-1.amazonaws.com
+/usr/bin/aws ecr get-login-password --region us-east-1 | /usr/bin/docker login --username AWS --password-stdin 571888835380.dkr.ecr.us-east-1.amazonaws.com
 
 # Pull the Docker image from ECR
 /usr/bin/docker pull 571888835380.dkr.ecr.us-east-1.amazonaws.com/python-repo:latest
