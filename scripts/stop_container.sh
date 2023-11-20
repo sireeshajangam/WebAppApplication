99941340719b25f8d1eba2ddd9f18b8bcbd6bf26
sireeshajangam <sireesha.jangam@auropro.com>
2023-11-20 12:54:22 +0530

    committed

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
 set -e
 
-stop_containers() {
-    running_containers=$(sudo docker ps -q --filter "expose=8000" || true)
+# Get IDs of all running containers
+running_containers=$(sudo docker ps -q)
 
-    # Check if there are any running containers to stop
-    if [ -n "$running_containers" ]; then
-        echo "Containers to stop:"
-        echo "$running_containers"
+# Check if there are any running containers to stop
+if [ -n "$running_containers" ]; then
+    echo "Containers to stop:"
+    echo "$running_containers"
 
-        # Stop each container
-        while IFS= read -r container_id; do
-            echo "Stopping container $container_id..."
-            sudo docker stop "$container_id"
-        done <<< "$running_containers"
-    else
-        echo "No running containers exposing port 8000 found."
-    fi
-}
-
-# Call function to stop containers
-stop_containers
+    # Stop each container
+    for container_id in $running_containers; do
+        echo "Stopping container $container_id..."
+        sudo docker stop "$container_id"
+    done
+else
+    echo "No running containers found."
+fi
