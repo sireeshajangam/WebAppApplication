bef0a07bad140628bd3e9b81d62e7550902cc412
sireeshajangam <sireesha.jangam@auropro.com>
2023-11-21 20:25:08 +0530

    committed

--- app.py
+++ app.py
@@ -1,53 +1,71 @@
 from flask import Flask, render_template, request, redirect
 import mysql.connector
 import boto3
-import botocore
 import json
 
 app = Flask(__name__)
 
-DATABASES = {}  # Define DATABASES at the module level
+# Create a Secrets Manager client
+secrets_manager_client = boto3.client(service_name='secretsmanager', region_name='us-east-1')
 
-try:
-    # Create a Secrets Manager client
-    secrets_manager_client = boto3.client(service_name='secretsmanager', region_name='us-east-1')
+# Retrieve the secret values from AWS Secrets Manager
+secret_name = 'DBCredentials1'
+get_secret_value_response = secrets_manager_client.get_secret_value(SecretId=secret_name)
+secret_values = json.loads(get_secret_value_response['SecretString'])
 
-    secret_name = 'DBCredentials1'
-    get_secret_value_response = secrets_manager_client.get_secret_value(SecretId=secret_name)
+# Define the DATABASES dictionary using the retrieved secret values
+db_config = {
+    'host': secret_values.get('host', ''),
+    'database': secret_values.get('dbname', ''),
+    'user': secret_values.get('username', ''),
+    'password': secret_values.get('password', ''),
+}
 
-    secret_values = json.loads(get_secret_value_response['SecretString'])
+# Create a 'guestbook' table in MySQL with columns 'id', 'name', 'message', and 'timestamp'
+def create_guestbook_table():
+    conn = mysql.connector.connect(**db_config)
+    cursor = conn.cursor()
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS guestbook (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            name VARCHAR(255) NOT NULL,
+            message TEXT NOT NULL,
+            timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )
+    """)
+    conn.close()
 
-    # Define the DATABASES dictionary using the retrieved secret values
-    DATABASES = {
-        'default': {
-            'host': secret_values.get('host', ''),
-            'database': secret_values.get('dbname', ''),
-            'user': secret_values.get('username', ''),
-            'password': secret_values.get('password', ''),
-        }
-    }
+# Ensure the 'guestbook' table exists
+create_guestbook_table()
 
-    # Ensure the 'create_guestbook_table()' function comes after the try-except block
-    def create_guestbook_table():
-        conn = mysql.connector.connect(**DATABASES['default'])
-        cursor = conn.cursor()
-        cursor.execute("""
-            CREATE TABLE IF NOT EXISTS guestbook (
-                id INT AUTO_INCREMENT PRIMARY KEY,
-                name VARCHAR(255) NOT NULL,
-                message TEXT NOT NULL,
-                timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-            )
-        """)
-        conn.close()
+@app.route('/')
+def index():
+    messages = get_messages()
+    return render_template('index.html', messages=messages)
 
-    create_guestbook_table()
+@app.route('/add', methods=['GET', 'POST'])
+def add_message():
+    if request.method == 'POST':
+        name = request.form['name']
+        message_text = request.form['message']
+        add_message_to_db(name, message_text)
+        return redirect('/')
+    return render_template('add_message.html')
 
-except (botocore.exceptions.ClientError, KeyError) as e:
-    print(f"Error fetching secrets: {e}")
+def get_messages():
+    conn = mysql.connector.connect(**db_config)
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM guestbook ORDER BY timestamp DESC")
+    messages = cursor.fetchall()
+    conn.close()
+    return messages
 
-# Routes and other functions
-# ...
+def add_message_to_db(name, message_text):
+    conn = mysql.connector.connect(**db_config)
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO guestbook (name, message) VALUES (%s, %s)", (name, message_text))
+    conn.commit()
+    conn.close()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
