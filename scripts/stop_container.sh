65c417e0b1b784d791775142f27f497fdad8b204
sireeshajangam <sireesha.jangam@auropro.com>
2023-11-26 23:35:31 +0530

    commiited

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -4,7 +4,12 @@ set -e
 # Get IDs of all running containers
 running_containers=$(sudo docker ps -q)
 
-# Loop through each container ID and stop it
-for container_id in $running_containers; do
-    sudo docker stop "$container_id"
-done
+# Check if there are running containers before attempting to stop
+if [ -n "$running_containers" ]; then
+    # Loop through each container ID and stop it
+    for container_id in $running_containers; do
+        sudo docker stop "$container_id"
+    done
+else
+    echo "No running containers found."
+fi
